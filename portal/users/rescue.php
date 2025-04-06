2a6c74e110a1db657b250cdbd2e66881afce8edb
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-07 00:47:16 +0800

    Update rescue.php

--- portal/users/rescue.php
+++ portal/users/rescue.php
@@ -11,8 +11,12 @@
                         <li>
                             <a href="report-emergency.php"><i class="fa fa-heartbeat"></i> <span>Report Emergency</span></a>
                         </li> 
-                        <li>
-                            <a href="view-emergency.php"><i class="fa fa-file"></i> <span>Emergency</span></a>
+                        <?php
+                        $result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending' ORDER BY id DESC ");
+                        $result->execute();
+                        for($i=0; $row = $result->fetch(); $i++){ ?>
+                    <li><a href="view-emergency.php"><i class="fa fa-file"></i> <span>Emergency</span> <span class="badge badge-pill btn-primary float-right"><?php echo $row['total'] ;?></span></a></li>
+                    <?php } ?>
                         </li>
                         <li>
                             <a href="announcement.php"><i class="fa fa-bell"></i> <span>Announcements</span></a>
