f0e4b7ae585c814ddc361a352a6976339ee31dee
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-06 22:42:04 +0800

    connect.php
    edited

--- connect.php
+++ connect.php
@@ -1,41 +1,35 @@
 <?php
 
+$db_host     = "srv1637.hstgr.io";
+$db_user     = "u665838367_cjmedDB";
+$db_pass     = "DBcjmed_2025!";
+$db_database = "u665838367_cjmed";
 
-$db_host        = "localhost";
-$db_user        = "root";
-$db_pass        = "";
-$db_database    = "ems";
 include "idiorm.php";
-/* End config */
- ORM::configure("mysql:host=".$db_host.";dbname=".$db_database);
- ORM::configure("username",$db_user);
- ORM::configure("password",$db_pass);
 
+/* ORM config */
+ORM::configure("mysql:host=".$db_host.";dbname=".$db_database);
+ORM::configure("username", $db_user);
+ORM::configure("password", $db_pass);
 
-$db = new PDO("mysql:host=".$db_host.";dbname=".$db_database, $db_user, $db_pass);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$conn = mysqli_connect($db_host, $db_user,$db_pass);
-mysqli_select_db($conn, $db_database);
-$mysqli = new mysqli;
+try {
+    $db = new PDO("mysql:host=$db_host;dbname=$db_database", $db_user, $db_pass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
 
-
-class App {   
-    public static function message($type,$message,$code=''){
-        if($type=='error'){
-            return '<div class="alert alert-danger alert-dismissable">
-                                <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
-                               '.$message.' <a class="alert-link" href="#">'.$code.'</a>.
-                            </div>';
-        }else{
-             return '<div class="alert alert-success alert-dismissable">
-                                <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
-                               '.$message.' <a class="alert-link" href="#">'.$code.'</a>.
-                            </div>';
-        }
+/* Optional class */
+class App {
+    public static function message($type, $message, $code = '') {
+        $alertClass = $type === 'error' ? 'alert-danger' : 'alert-success';
+        return '<div class="alert '.$alertClass.' alert-dismissable">
+                    <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+                    '.$message.' <a class="alert-link" href="#">'.$code.'</a>.
+                </div>';
     }
 }
-function get($val){
+
+function get($val) {
     return @$_GET[$val];
 }
-    
-
