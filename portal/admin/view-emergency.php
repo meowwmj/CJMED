47276058a42cf42a062afa424848f89c3c7256f8
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-08 02:16:51 +0800

    Update view-emergency.php

--- portal/admin/view-emergency.php
+++ portal/admin/view-emergency.php
@@ -7,29 +7,50 @@
             <div class="sidebar-inner slimscroll">
                 <div id="sidebar-menu" class="sidebar-menu">
                     <ul>
-                        <li class=""><a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-                        <li><a href="announcement.php"><i class="fa fa-bell"></i> <span>Announcements</span></a></li>
-                        <li><a href="agency.php"><i class="fa fa-user-md"></i> <span>Agency</span></a></li>
-                        <li><a href="report-emergency.php"><i class="fa fa-heartbeat"></i> <span>Reports Emergency</span></a></li>
-                        <li class="active">
-                            <?php
-                            $result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending' ORDER BY id DESC ");
-                            $result->execute();
-                            for($i=0; $row = $result->fetch(); $i++){
-                            ?>
-                            <li class="active"><a href="view-emergency.php"><i class="fa fa-file"></i> <span>Emergency</span> <span class="badge badge-pill bg-primary float-right"><?php echo $row['total']; ?></span></a></li>
-                            <?php } ?>
+
+                        <li class="">
+                            <a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
                         </li>
-                        <li><a href="report_history.php"><i class="fa fa-file-text-o"></i> <span>History</span></a></li>
-                        <li><a href="view-archived-emergencies.php"><i class="fa fa-file"></i> <span>Archived</span></a></li>
-                        <li><a href="users.php"><i class="fa fa-user-plus"></i> <span>Manage Admin</span></a></li>
-                        <li><a href="users1.php"><i class="fa fa-user"></i> <span>Manage Users</span></a></li>
-                        <li><a href="rescue.php"><i class="fa fa-calendar-o"></i> <span>Rescue</span></a></li>
-                        <li><a href="logout.php"><i class="fa fa-power-off"></i> <span>Logout</span></a></li>
+                        <li>
+                            <a href="announcement.php"><i class="fa fa-bell"></i> <span>Announcements</span></a>
+                        </li>
+                        <li>
+                            <a href="agency.php"><i class="fa fa-user-md"></i> <span>Agency</span></a>
+                        </li>
+                        <li>
+                            <a href="report-emergency.php"><i class="fa fa-heartbeat"></i> <span>Reports Emergency</span></a>
+                        </li>
+                        <li class="active">
+                        <?php
+                        $result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending' ORDER BY id DESC ");
+                        $result->execute();
+                        for($i=0; $row = $result->fetch(); $i++){ ?>
+                        <li class="active"><a href="view-emergency.php"><i class="fa fa-file"></i> <span>Emergency</span> <span class="badge badge-pill bg-primary float-right"><?php echo $row['total'] ;?></span></a></li>
+                        <?php } ?>
+                        </li>
+                        
+                        <li>
+                            <a href="report_history.php"><i class="fa fa-file-text-o"></i> <span>History</span></a>
+                        </li>
+                        <li>                          
+                            <a href="view-archived-emergencies.php"><i class="fa fa-file"></i> <span>Archived</span></a>
+                        </li>
+                        <li>
+                            <a href="users.php"><i class="fa fa-user-plus"></i> <span>Manage Admin</span></a>
+                        </li>
+                        <li>
+                            <a href="users1.php"><i class="fa fa-user"></i> <span>Manage Users</span></a>
+                        </li>
+                        <li>
+                            <a href="rescue.php"><i class="	fa fa-calendar-o"></i> <span>Rescue</span></a> 
+                        </li>
+                        <li>
+                            <a href="logout.php"><i class="fa fa-power-off"></i> <span>Logout</span></a>
+                        </li>
                     </ul>
                 </div>
             </div>
-        </div>
+        </div>    
     </div>
 
     <!-- Page Wrapper -->
