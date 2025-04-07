d35c9431964aa8e54e1a3cf3ce05b89af44c5348
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-08 02:01:57 +0800

    Update rescue.php

--- portal/agency/rescue.php
+++ portal/agency/rescue.php
@@ -3,30 +3,40 @@
 <body>
     <div class="main-wrapper">
         <?php include 'includes/navigation.php'; ?>
-        <?php include 'includes/sidebar.php'; ?>    
         <div class="sidebar" id="sidebar">
             <div class="sidebar-inner slimscroll">
                 <div id="sidebar-menu" class="sidebar-menu">
                     <ul>
                         <li>
-                            <a href="report-emergency.php"><i class="fa fa-heartbeat"></i> <span>Report Emergency</span></a>
-                        </li> 
-                        <?php
-                        $result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending' ORDER BY id DESC ");
-                        $result->execute();
-                        for($i=0; $row = $result->fetch(); $i++){ ?>
-                    <li><a href="view-emergency.php"><i class="fa fa-file"></i> <span>Emergency</span> <span class="badge badge-pill btn-primary float-right"><?php echo $row['total'] ;?></span></a></li>
-                    <?php } ?>
+                            <a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
                         </li>
                         <li>
                             <a href="announcement.php"><i class="fa fa-bell"></i> <span>Announcements</span></a>
-                        </li>           
-                        <li>
-                            <a href="report_history.php"><i class="fa fa-file-text-o"></i> <span>History</span></a>
                         </li>
                         <li class="active">
-                            <a href="rescue.php"><i class="fa fa-calendar-o"></i> <span>Rescue</span></a> 
+                            <a href="report-emergency.php"><i class="fa fa-heartbeat"></i> <span>Report Emergency</span></a>
                         </li>
+                        <?php
+                        // include('../connect.php');
+                        $result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending'");
+                        $result->execute();
+                        for($i=0; $row = $result->fetch(); $i++){
+                        ?>  
+                        <li>
+                            <a href="view-emergency.php"><i class="fa fa-file"></i> <span>Emergency</span> <span class="badge badge-pill btn-primary float-right"><?php echo $row['total'] ;?></span></a>
+                        </li>
+                    <?php } ?>
+                        <li >
+                            <a href="report_history.php"><i class="fa fa-file-text-o"></i> <span>History</span></a>
+                        </li>
+                        <li>
+                            <a href="profile.php"><i class="fa fa-user"></i> <span>Profile</span></a>
+                        </li>
+                        
+                         <li>
+                            <a href="rescue.php"><i class="	fa fa-calendar-o"></i> <span>Rescue</span></a> 
+                        </li>
+
                         <li>
                             <a href="logout.php"><i class="fa fa-power-off"></i> <span>Logout</span></a>
                         </li>
