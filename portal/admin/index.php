323cbb3f9b7e2171e276782526cd1f68e37f32c0
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-08 06:39:08 +0800

    Update index.php

--- portal/admin/index.php
+++ portal/admin/index.php
@@ -200,7 +200,7 @@
 
 <style>
     .table-responsive {
-        max-height: 770px; /* Adjust this height as needed */
+        max-height: 370px; /* Adjust this height as needed */
         overflow-y: auto;
     }
 </style>
