33ed255bc564affbbbf7050986fe5653606e86f8
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-07 23:14:36 +0800

    Update index.php

--- portal/agency/index.php
+++ portal/agency/index.php
@@ -59,64 +59,62 @@
                     <?php } ?>
                     </div>
                 </div>
-				
-				<div class="row">
-					<div class="col-12 col-md-6 col-lg-8 col-xl-8">
-						<div class="card">
-							<div class="card-header">
-								<h4 class="card-title d-inline-block">Agency</h4> <a href="agency.php" class="btn btn-primary float-right">View all</a>
-							</div>
-							<div class="card-body p-0">
-								<div class="table-responsive">
-									<table class="table mb-0">
-										<thead class="">
-											<tr>
-												<th>Agency</th>
-												<th>Contact</th>
-												<th>Person In Charge</th>
-												
-											</tr>
-										</thead>
-										<tbody>
-											<?php
-			                $result = $db->prepare("SELECT * FROM agency ORDER BY id DESC Limit 5");
-			                $result->execute();
-			                for($i=1; $row = $result->fetch(); $i++){ 
-			               
-			               ?> 
-											<tr>
-												<td style="min-width: 200px;">
-													 <a href="#" title=""><img src="../../uploads/<?php echo $row['photo']; ?>" alt="" class="w-40 rounded-circle"></a>
-													<h2><a href="#"><a href="agency_profile.php?id=<?php echo $row['id'];?>"><?php echo $row['agency_name']; ?>  <span><?php echo $row['address']; ?></span></a></h2>
-												</td>                 
-												<td>
-													<h5 class="time-title p-0"><?php echo $row['email']; ?></h5>
-													<p><?php echo $row['phone_number']; ?></p>
-												</td>
-												<td>
-													<h5 class="time-title p-0"><?php echo $row['personincharge']; ?></h5>
-													<!-- <p>7.00 PM</p> -->
-												</td>
-												
-											</tr>
-											<?php } ?>	
-										</tbody>
-									</table>
-								</div>
-							</div>
-						</div>
-					</div>
+
+   		<div class="row">
+                    <div class="col-12 col-md-6 col-lg-8 col-xl-8">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title d-inline-block">Agency</h4>
+                                <a href="agency.php" class="btn btn-primary float-right">View all</a>
+                            </div>
+                            <div class="card-body p-0">
+                                <div class="table-responsive">
+                                    <table class="table table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>Agency</th>
+                                                <th>Contact</th>
+                                                <th>Person In Charge</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                                $result = $db->prepare("SELECT * FROM agency ORDER BY id DESC LIMIT 5");
+                                                $result->execute();
+                                                while($row = $result->fetch()) {
+                                            ?>
+                                            <tr>
+                                                <td>
+                                                    <img src="../../uploads/<?php echo $row['photo']; ?>" alt="Agency Image" class="rounded-circle" width="40" height="40">
+                                                    <a href="agency_profile.php?id=<?php echo $row['id']; ?>"><?php echo $row['agency_name']; ?></a>
+                                                    <br><span><?php echo $row['address']; ?></span>
+                                                </td>
+                                                <td>
+                                                    <p><?php echo $row['email']; ?></p>
+                                                    <p><?php echo $row['phone_number']; ?></p>
+                                                </td>
+                                                <td>
+                                                    <p><?php echo $row['personincharge']; ?></p>
+                                                </td>
+                                            </tr>
+                                            <?php } ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <div class="col-12 col-md-6 col-lg-4 col-xl-4">
                         <div class="card member-panel">
-							<div class="card-header bg-white">
-								<h4 class="card-title mb-0">USERS</h4>
-							</div>
+				<div class="card-header bg-white">
+					<h4 class="card-title mb-0">USERS</h4>
+				</div>
                             <div class="card-body">
 			                 	<?php
 			                $result = $db->prepare("SELECT * FROM users ");
 			                $result->execute();
-			                for($i=1; $row = $result->fetch(); $i++){ 
-			               
+			                for($i=1; $row = $result->fetch(); $i++){   
 			               ?> 
                                 <ul class="contact-list">
                                     <li>
@@ -129,8 +127,7 @@
                                                 <span class="contact-date"><?php echo $row['phone']; ?></span>
                                             </div>
                                         </div>
-                                    </li>
-                                    
+                                    </li>   
                                 </ul>
                             <?php } ?>
                             </div>
@@ -140,7 +137,7 @@
                             </div>
                         </div>
                     </div>
-				</div>
+		</div>
             </div>
             <?php include 'includes/Message.php'; ?>
         </div>
@@ -167,7 +164,6 @@
   </div>
 </div>
 
-
     <div class="sidebar-overlay" data-reff=""></div>
     <script src="assets/js/jquery-3.2.1.min.js"></script>
 	<script src="assets/js/popper.min.js"></script>
@@ -177,9 +173,5 @@
     <script src="assets/js/chart.js"></script>
     <script src="assets/js/app.js"></script>
 
-
-
-
 </body>
 </html>
-
