1b62cd2075b1b4c82a4c92720bcc3d8816c924b6
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-08 01:28:51 +0800

    Update index.php

--- portal/admin/index.php
+++ portal/admin/index.php
@@ -99,19 +99,18 @@
 			                for($i=1; $row = $result->fetch(); $i++){ 
 			               
 			               ?> 
-											<tr>
-													<td style="min-width: 200px;">
-                                                    <span class="status online"></span></a>
-														<h2><a href="#"><?php echo $row['agency_name']; ?> <span><?php echo $row['state']; ?></span></a></h2>
-													</td>                 
-													<td>
-														<h5 class="time-title p-0"><?php echo $row['email']; ?></h5>
-														<p><?php echo $row['phone_number']; ?></p>
-													</td>
-													<td>
-														<h5 class="time-title p-0"><?php echo $row['personincharge']; ?></h5>
+					   <tr>
+						<td style="min-width: 200px;">                                                    
+							<h2><a href="#"><?php echo $row['agency_name']; ?> <span><?php echo $row['state']; ?></span></a></h2>
+								</td>                 
+								<td>
+									<h5 class="time-title p-0"><?php echo $row['email']; ?></h5>
+									<p><?php echo $row['phone_number']; ?></p>
+									</td>
+										<td>
+											<h5 class="time-title p-0"><?php echo $row['personincharge']; ?></h5>
 														<!-- <p>7.00 PM</p> -->
-													</td>
+												</td>
 													
 												</tr>
 											<?php } ?>	
