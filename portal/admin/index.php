18cf254fa9853048cf5b9485e7e528586b241c61
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-08 21:35:30 +0800

    Update index.php

--- portal/admin/index.php
+++ portal/admin/index.php
@@ -1,3 +1,5 @@
+
+
 <?php include 'includes/head.php'; ?>
 <body>
     <div class="main-wrapper">
@@ -6,186 +8,187 @@
         <div class="page-wrapper">
             <div class="content">
                 <div class="row">
-                    <div class="col-6 col-sm-6 col-md-3 col-xl-3">
+                    <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
                     	<?php
-                        // include('../connect.php');
-                        $result = $db->prepare("SELECT count(*) as total FROM emergency");
-                        $result->execute();
-                        for($i=0; $row = $result->fetch(); $i++){
-                        ?>
-                            <a href="view-emergency.php">
-                                <div class="dash-widget">
-                                    <span class="dash-widget-bg1"><i class="fa fa-stethoscope" aria-hidden="true"></i></span>
-                                    <div class="dash-widget-info text-right">
-                                        <h3><?php echo $row['total']; ?></h3>
-                                        <span class="widget-title1">Emergency <i class="fa fa-check" aria-hidden="true"></i></span>
-                                    </div>
-                                </div>
-                            </a>
+		                // include('../connect.php');
+				$result = $db->prepare("SELECT count(*) as total FROM emergency");
+				$result->execute();
+				for($i=0; $row = $result->fetch(); $i++){
+		        ?>
+                         <a href="view-emergency.php"><div class="dash-widget">
+				<span class="dash-widget-bg1"><i class="fa fa-stethoscope" aria-hidden="true"></i></span>
+					<div class="dash-widget-info text-right">
+						<h3><?php echo $row['total']; ?></h3>
+				<span class="widget-title1">Emergency <i class="fa fa-check" aria-hidden="true"></i></span>
+				</div>
+                        </div>
                         <?php } ?>
                     </div>
                 
-                    <div class="col-6 col-sm-6 col-md-3 col-xl-3">
+                    <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
                     	<?php
-                        // include('../connect.php');
-                        $result = $db->prepare("SELECT count(*) as total FROM users");
-                        $result->execute();
-                        for($i=0; $row = $result->fetch(); $i++){
-                        ?>
-                            <a href="users1.php">
-                                <div class="dash-widget">
-                                    <span class="dash-widget-bg2"><i class="fa fa-user-o"></i></span>
-                                    <div class="dash-widget-info text-right">
-                                        <h3><?php echo $row['total']; ?></h3>
-                                        <span class="widget-title2">Users <i class="fa fa-check" aria-hidden="true"></i></span>
-                                    </div>
-                                </div>
-                            </a>
-                        <?php } ?>
+		                // include('../connect.php');
+						$result = $db->prepare("SELECT count(*) as total FROM users");
+						$result->execute();
+						for($i=0; $row = $result->fetch(); $i++){
+		                ?>
+                        <a href="users1.php"><div class="dash-widget">
+                            <span class="dash-widget-bg2"><i class="fa fa-user-o"></i></span>
+                            <div class="dash-widget-info text-right">
+                                <h3><?php echo $row['total'] ;?></h3>
+                                <span class="widget-title2">Users <i class="fa fa-check" aria-hidden="true"></i></span>
+                            </div>
+                        </div>
+                    <?php } ?>
                     </div>
 
-                    <div class="col-6 col-sm-6 col-md-3 col-xl-3">
+                    <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
                     	<?php
-                        // include('../connect.php');
-                        $result = $db->prepare("SELECT count(*) as total FROM agency");
-                        $result->execute();
-                        for($i=0; $row = $result->fetch(); $i++){
-                        ?>
-                            <a href="agency.php">
-                                <div class="dash-widget">
-                                    <span class="dash-widget-bg3"><i class="fa fa-user-md" aria-hidden="true"></i></span>
-                                    <div class="dash-widget-info text-right">
-                                        <h3><?php echo $row['total']; ?></h3>
-                                        <span class="widget-title3">Agency <i class="fa fa-check" aria-hidden="true"></i></span>
-                                    </div>
-                                </div>
-                            </a>
-                        <?php } ?>
+		                // include('../connect.php');
+						$result = $db->prepare("SELECT count(*) as total FROM agency");
+						$result->execute();
+						for($i=0; $row = $result->fetch(); $i++){
+		                ?>
+                        <a href="agency.php"><div class="dash-widget">
+                            <span class="dash-widget-bg3"><i class="fa fa-user-md" aria-hidden="true"></i></span>
+                            <div class="dash-widget-info text-right">
+                                <h3><?php echo $row['total'] ;?></h3>
+                                <span class="widget-title3">Agency <i class="fa fa-check" aria-hidden="true"></i></span>
+                            </div>
+                        </div>
+                    <?php } ?>
                     </div>
-
-                    <div class="col-6 col-sm-6 col-md-3 col-xl-3">
+                    <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
                     	<?php
-                        // include('../connect.php');
-                        $result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending'");
-                        $result->execute();
-                        for($i=0; $row = $result->fetch(); $i++){
-                        ?>
-                            <a href="view-emergency.php">
-                                <div class="dash-widget">
-                                    <span class="dash-widget-bg4"><i class="fa fa-heartbeat" aria-hidden="true"></i></span>
-                                    <div class="dash-widget-info text-right">
-                                        <h3><?php echo $row['total']; ?></h3>
-                                        <span class="widget-title4">Ongoing <i class="fa fa-check" aria-hidden="true"></i></span>
-                                    </div>
-                                </div>
-                            </a>
-                        <?php } ?>
+		                // include('../connect.php');
+						$result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending'");
+						$result->execute();
+						for($i=0; $row = $result->fetch(); $i++){
+		                ?>
+                        <a href="view-emergency.php"><div class="dash-widget">
+                            <span class="dash-widget-bg4"><i class="fa fa-heartbeat" aria-hidden="true"></i></span>
+                            <div class="dash-widget-info text-right">
+                                <h3><?php echo $row['total'] ;?></h3>
+                                <span class="widget-title4">Ongoing <i class="fa fa-check" aria-hidden="true"></i></span>
+                            </div>
+                        </div>
+                    <?php } ?>
                     </div>
                 </div>
 				
-                <div class="row">
-                    <div class="col-12 col-md-12 col-lg-8 col-xl-8">
-                        <div class="card">
-                            <div class="card-header">
-                                <h4 class="card-title d-inline-block">Agency</h4> <a href="agency.php" class="btn btn-primary float-right">View all</a>
-                            </div>
-                            <div class="card-body p-0">
-                                <div class="table-responsive">
-                                    <table class="table mb-0">
-                                        <thead>
-                                            <tr>
-                                                <th>Agency</th>
-                                                <th>Contact</th>
-                                                <th>Person In Charge</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                                $result = $db->prepare("SELECT * FROM agency ORDER BY id DESC Limit 5");
-                                                $result->execute();
-                                                for($i=1; $row = $result->fetch(); $i++){ 
-                                            ?> 
-                                            <tr>
-                                                <td style="min-width: 12rem;">
-                                                    <span class="status online"></span>
-                                                    <h2><a href="#"><?php echo $row['agency_name']; ?> <span><?php echo $row['state']; ?></span></a></h2>
-                                                </td>                 
-                                                <td>
-                                                    <h5 class="time-title p-0"><?php echo $row['email']; ?></h5>
-                                                    <p><?php echo $row['phone_number']; ?></p>
-                                                </td>
-                                                <td>
-                                                    <h5 class="time-title p-0"><?php echo $row['personincharge']; ?></h5>
-                                                </td>
-                                            </tr>
-                                            <?php } ?>	
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+          <div class="row">
+					<div class="col-12 col-md-12 col-lg-8 col-xl-8">
+						<div class="card">
+							<div class="card-header">
+								<h4 class="card-title d-inline-block">Agency</h4> <a href="agency.php" class="btn btn-primary float-right">View all</a>
+							</div>
+							<div class="card-body p-0">
+								<div class="table-responsive">
+									<table class="table mb-0">
+										<thead class="">
+											<tr>
+												<th>Agency</th>
+												<th>Contact</th>
+												<th>Person In Charge</th>
+												
+											</tr>
+										</thead>
+										<tbody>
+											<?php
+			                $result = $db->prepare("SELECT * FROM agency ORDER BY id DESC Limit 5");
+			                $result->execute();
+			                for($i=1; $row = $result->fetch(); $i++){ 
+			               
+			               ?> 
+											<tr>
+													<td style="min-width: 200px;">
+                                                    <span class="status online"></span></a>
+														<h2><a href="#"><?php echo $row['agency_name']; ?> <span><?php echo $row['state']; ?></span></a></h2>
+													</td>                 
+													<td>
+														<h5 class="time-title p-0"><?php echo $row['email']; ?></h5>
+														<p><?php echo $row['phone_number']; ?></p>
+													</td>
+													<td>
+														<h5 class="time-title p-0"><?php echo $row['personincharge']; ?></h5>
+														<!-- <p>7.00 PM</p> -->
+													</td>
+													
+												</tr>
+											<?php } ?>	
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+                    
+                    
                     
                     <div class="col-12 col-md-12 col-lg-4 col-xl-4">
                         <div class="card member-panel">
-                            <div class="card-header bg-white">
-                                <h4 class="card-title mb-0">USERS</h4>
-                            </div>
-                            <div class="card-body p-0">
-                                <table class="table table-striped">
-                                    <?php
-                                    $result = $db->prepare("SELECT * FROM users");
-                                    $result->execute();
-                                    for($i=1; $row = $result->fetch(); $i++){ 
-                                    ?> 
-                                    <ul class="contact-list">
-                                        <li>
-                                            <div class="contact-cont">
-                                                <div class="float-left user-img m-r-10">
-                                                    <a href="#" title="John Doe"><img src="../../uploads/<?php echo $row['photo']; ?>" alt="" class="w-40 rounded-circle"></a>
-                                                </div>
-                                                <div class="contact-info">
-                                                    <span class="contact-name text-ellipsis"><?php echo $row['username']; ?></span>
-                                                    <span class="contact-date"><?php echo $row['phone']; ?></span>
-                                                </div>
+			    <div class="card-header bg-white">
+				<h4 class="card-title mb-0">USERS</h4>
+		    </div>
+                        <div class="card-body p-0">
+                        <table class="table table-striped">
+		    <?php
+		        $result = $db->prepare("SELECT * FROM users ");
+			 $result->execute();
+			for($i=1; $row = $result->fetch(); $i++){ 
+		     ?> 
+                                <ul class="contact-list">
+                                    <li>
+                                        <div class="contact-cont">
+                                            <div class="float-left user-img m-r-10">
+                                                <a href="#" title="John Doe"><img src="../../uploads/<?php echo $row['photo']; ?>" alt="" class="w-40 rounded-circle"></span></a>
                                             </div>
-                                        </li>
-                                    </ul>
-                                    <?php } ?>
-                                </table>
+                                            <div class="contact-info">
+                                                <span class="contact-name text-ellipsis"><?php echo $row['username']; ?></span>
+                                                <span class="contact-date"><?php echo $row['phone']; ?></span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    
+                                </ul>
+                            <?php } ?>
+                            </div>
+							
+                            <!--<div class="card-footer text-center bg-white">
+                                <a href="add_user" class="text-muted">Add Users</a> -->
                             </div>
                         </div>
                     </div>
-                </div>
+				</div>
             </div>
             <?php include 'includes/Message.php'; ?>
         </div>
     </div>
-
+    
     <!-- Logout Confirmation Modal -->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="logoutModalLabel">Confirm Logout</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    Are you sure you want to log out?
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <a href="logout.php" class="btn btn-danger">Logout</a>
-                </div>
-            </div>
-        </div>
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="logoutModalLabel">Confirm Logout</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Are you sure you want to log out?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        <a href="logout.php" class="btn btn-danger">Logout</a>
+      </div>
     </div>
+  </div>
+</div>
 
     <div class="sidebar-overlay" data-reff=""></div>
     <script src="assets/js/jquery-3.2.1.min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/jquery.slimscroll.js"></script>
     <script src="assets/js/Chart.bundle.js"></script>
@@ -197,25 +200,7 @@
 
 <style>
     .table-responsive {
-        max-height: 29.375rem; /* 470px converted to rem (1rem = 16px) */
+        max-height: 470px; /* Adjust this height as needed */
         overflow-y: auto;
-        overflow-x: auto;
-    }
-
-    .dash-widget {
-        padding: 1rem;
-        box-sizing: border-box;
-    }
-
-    @media (max-width: 48rem) { /* 768px */
-        .dash-widget {
-            margin-bottom: 1.25rem;
-        }
-
-        .user-img img {
-            width: 2.5rem; /* 40px */
-            height: auto;
-            max-width: 100%;
-        }
     }
 </style>
