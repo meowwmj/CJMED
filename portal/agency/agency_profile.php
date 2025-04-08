79f6a2c867e7025b30e4a0ec2846331c0c786ada
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-08 08:56:20 +0800

    Update agency_profile.php

--- portal/agency/agency_profile.php
+++ portal/agency/agency_profile.php
@@ -1,57 +1,9 @@
 <?php include 'includes/head.php'; ?>
-
 <body>
     <div class="main-wrapper">
         <?php include 'includes/navigation.php'; ?>
-        <div class="sidebar" id="sidebar">
-            <div class="sidebar-inner slimscroll">
-                <div id="sidebar-menu" class="sidebar-menu">
-                    <ul>
-                        <li>
-                            <a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
-                        </li>
-                        <li class="">
-                            <a href="announcement.php"><i class="fa fa-bell"></i> <span>Announcements</span></a>
-                        </li>
-                        <li class="active">
-                            <a href="agency.php"><i class="fa fa-user-md"></i> <span>Agency</span></a>
-                        </li>
-                        <li>
-                            <a href="report-emergency.php"><i class="fa fa-heartbeat"></i> <span>Reports Emergency</span></a>
-                        </li>
-                        <?php
-                        // include('../connect.php');
-                        $result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending'");
-                        $result->execute();
-                        for($i=0; $row = $result->fetch(); $i++){
-                        ?>  
-                        <li>
-                            <a href="view-emergency.php"><i class="fa fa-file"></i> <span>Emergency</span> <span class="badge badge-pill bg-primary float-right"><?php echo $row['total'] ;?></span></a>
-                        </li>
-                    <?php } ?>
-                        
-                        <li>
-                            <a href="report_history.php"><i class="fa fa-file-text-o"></i> <span>History</span></a>
-                        </li>
-                        <li>                          
-                            <a href="view-archived-emergencies.php"><i class="fa fa-file"></i> <span>Archived</span></a>
-                        </li>
-                        <li>
-                            <a href="users.php"><i class="fa fa-user-plus"></i> <span>Manage Admin</span></a>
-                        </li>
-                        <li>
-                            <a href="users1.php"><i class="fa fa-user"></i> <span>Manage Users</span></a>
-                        </li>
-                        <li>
-                            <a href="rescue.php"><i class="	fa fa-calendar-o"></i> <span>Rescue</span></a> 
-                        </li>
-                        <li>
-                            <a href="logout.php"><i class="fa fa-power-off"></i> <span>Logout</span></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>    
+        <?php include 'includes/sidebar.php'; ?>
+
         <div class="page-wrapper">
             <div class="content">
                 <div class="row">
@@ -60,7 +12,7 @@
                     </div>
                   <div class="col-sm-7 col-6 text-left m-b-30">
                         <div class="card member-panel">
-							<div class="card-header bg-blue">
+			<div class="card-header bg-blue">
                                 
               
             <tbody>
