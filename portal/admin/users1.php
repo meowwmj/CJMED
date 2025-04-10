9464bb172d9fdd09956783a4d0e304c0c4fa64c3
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-10 15:45:32 +0800

    Update users1.php

--- portal/admin/users1.php
+++ portal/admin/users1.php
@@ -33,10 +33,15 @@
                             <a href="view-archived-emergencies.php"><i class="fa fa-archive"></i> <span>Archived</span></a>
                         </li>
                         <li>
-                            <a href="users.php"><i class="fa fa-user-plus"></i> <span>Manage Admin</span></a>
-                        </li>
-                        <li class="active">
-                            <a href="users1.php"><i class="fa fa-user"></i> <span>Manage Users</span></a>
+                        <a href="#"><i class="fa fa-users"></i> <span>Manage</span> <span class="menu-arrow"></span></a>
+                        <ul class="submenu-list">
+                            <li>
+                                <a href="users.php"><i class="fa fa-user-plus"></i> <span>Manage Admin</span></a>
+                            </li>
+                            <li>
+                                <a href="users1.php"><i class="fa fa-user"></i> <span>Manage Users</span></a>                        
+                            </li>
+                        </ul>
                         </li>
                         <li>
                             <a href="rescue.php"><i class="	fa fa-calendar-o"></i> <span>Rescue</span></a> 
@@ -160,3 +165,33 @@
 <!-- add-appointment24:07-->
 </html>
 
+<style>
+.submenu-list {
+    list-style: none;
+    padding-left: 20px;
+    margin: 0; /* Remove any margin */
+    background-color: #2c3e50; /* Ensure the background blends with the sidebar */
+}   
+.submenu-list li {
+    margin: 0;
+    background-color: #2c3e50; 
+    padding: 0px;
+    padding-top: 10px;
+}
+
+.submenu-list a {
+    background-color: #2c3e50;
+    text-decoration: none;
+    font-size: 15px;
+    padding: 10px;
+    display: block;
+    border-radius: 25px;
+    transition: all 0.3s ease;
+}
+
+.submenu-list a:hover,
+.submenu-list .active a {
+    background-color: #fff;
+    color: #2c3e50;
+}
+</style>
