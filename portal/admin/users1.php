4f8eca093d5dde89b1d156e9df4337e0099bc363
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-10 08:56:57 +0800

    Update users1.php

--- portal/admin/users1.php
+++ portal/admin/users1.php
@@ -7,18 +7,15 @@
             <div class="sidebar-inner slimscroll">
                 <div id="sidebar-menu" class="sidebar-menu">
                     <ul>
-                        <li class="">
+                        <li class>
                             <a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
                         </li>
                         <li>
                             <a href="announcement.php"><i class="fa fa-bell"></i> <span>Announcements</span></a>
                         </li>
-                        <li class="">
+                        <li class>
                             <a href="agency.php"><i class="fa fa-user-md"></i> <span>Agency</span></a>
-                        </li>
-                        <li>
-                            <a href="report-emergency.php"><i class="fa fa-heartbeat"></i> <span>Reports Emergency</span></a>
-                        </li>
+                        </li>                     
                         <?php
                         // include('../connect.php');
                         $result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending'");
@@ -28,8 +25,7 @@
                         <li>
                             <a href="view-emergency.php"><i class="fa fa-file"></i> <span>Emergency</span> <span class="badge badge-pill btn-primary float-right"><?php echo $row['total'] ;?></span></a>
                         </li>
-                    <?php } ?>
-                        
+                    <?php } ?>                        
                         <li>
                             <a href="report_history.php"><i class="fa fa-file-text-o"></i> <span>History</span></a>
                         </li>
