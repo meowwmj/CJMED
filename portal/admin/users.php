59a39cfcb4e8a0bb349e84c16d0cb6870d402478
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-10 15:51:40 +0800

    Update users.php

--- portal/admin/users.php
+++ portal/admin/users.php
@@ -33,10 +33,9 @@
                             <a href="view-archived-emergencies.php"><i class="fa fa-archive"></i> <span>Archived</span></a>
                         </li>
                         <li class="active">
-			    <a href="#"><i class="fa fa-users"></i> <span>Manage</span> <span class="menu-arrow"></span></a>
-			    <ul class="submenu-list">
+			    <a href="#"><i class="fa fa-users"></i> <span>Manage</span> <span class="menu-arrow"></span></a>			   
 			</li>
-		        <li>
+		        <li> <ul class="submenu-list">
 			    <li>
 			        <a href="users.php"><i class="fa fa-user-plus"></i> <span>Manage Admin</span></a>
 			    </li>
