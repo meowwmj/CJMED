7d0a315140bd73814d46e01a82a6f02df1528c62
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-10 09:36:56 +0800

    Update rescue.php

--- portal/agency/rescue.php
+++ portal/agency/rescue.php
@@ -31,15 +31,10 @@
                         </li>
                         <li class>                          
                             <a href="view-archived-emergencies.php"><i class="fa fa-archive"></i> <span>Archived</span></a>
-                        </li>
-                        <li>
-                            <a href="profile.php"><i class="fa fa-user"></i> <span>Profile</span></a>
-                        </li>
-                        
+                        </li>                     
                          <li class="active">
                             <a href="rescue.php"><i class="	fa fa-calendar-o"></i> <span>Rescue</span></a> 
                         </li>
-
                         <li>
                             <a href="logout.php"><i class="fa fa-power-off"></i> <span>Logout</span></a>
                         </li>
