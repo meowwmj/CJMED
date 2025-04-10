023d4807452586fa0d061321403ffcd494c9ca8f
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-10 09:01:00 +0800

    Update make_action.php

--- portal/admin/make_action.php
+++ portal/admin/make_action.php
@@ -7,13 +7,13 @@
             <div class="sidebar-inner slimscroll">
                 <div id="sidebar-menu" class="sidebar-menu">
                     <ul>
-                        <li class="">
+                        <li class>
                             <a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
                         </li>
                         <li>
                             <a href="announcement.php"><i class="fa fa-bell"></i> <span>Announcements</span></a>
                         </li>
-                        <li class="">
+                        <li class>
                             <a href="agency.php"><i class="fa fa-user-md"></i> <span>Agency</span></a>
                         </li>
                         <?php
@@ -25,8 +25,7 @@
                         <li class="active">
                             <a href="view-emergency.php"><i class="fa fa-file"></i> <span>Emergency</span> <span class="badge badge-pill btn-primary float-right"><?php echo $row['total'] ;?></span></a>
                         </li>
-                    <?php } ?>
-                       
+                        <?php } ?>                       
                         <li>
                             <a href="report_history.php"><i class="fa fa-file-text-o"></i> <span>History</span></a>
                         </li>
