89920da40703f54898100ff56e3f7b377633e37a
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-10 08:57:22 +0800

    Update view-archived-emergencies.php

--- portal/admin/view-archived-emergencies.php
+++ portal/admin/view-archived-emergencies.php
@@ -7,7 +7,7 @@
             <div class="sidebar-inner slimscroll">
                 <div id="sidebar-menu" class="sidebar-menu">
                     <ul>
-                        <li class="">
+                        <li>
                             <a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
                         </li>
                         <li>
@@ -15,10 +15,7 @@
                         </li>
                         <li>
                             <a href="agency.php"><i class="fa fa-user-md"></i> <span>Agency</span></a>
-                        </li>
-                        <li>
-                            <a href="report-emergency.php"><i class="fa fa-heartbeat"></i> <span>Reports Emergency</span></a>
-                        </li>
+                        </li>                      
                         <?php
                         // Query to count pending emergencies
                         $result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending'");
