6ad5955f0d93714189d7b73e93f7deac54e93500
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-10 09:00:23 +0800

    Update emergency_type.php

--- portal/admin/emergency_type.php
+++ portal/admin/emergency_type.php
@@ -6,13 +6,13 @@
             <div class="sidebar-inner slimscroll">
                 <div id="sidebar-menu" class="sidebar-menu">
                     <ul>
-                        <li class="">
+                        <li class>
                             <a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
                         </li>
                         <li>
                             <a href="announcement.php"><i class="fa fa-bell"></i> <span>Announcements</span></a>
                         </li>
-                        <li class="">
+                        <li class>
                             <a href="agency.php"><i class="fa fa-user-md"></i> <span>Agency</span></a>
                         </li>                     
                         <?php
@@ -24,7 +24,7 @@
                         <li>
                             <a href="view-emergency.php"><i class="fa fa-file"></i> <span>Emergency</span> <span class="badge badge-pill btn-primary float-right"><?php echo $row['total'] ;?></span></a>
                         </li>
-                    <?php } ?>
+                    	<?php } ?>
                         <li>
                             <a href="report_history.php"><i class="fa fa-file-text-o"></i> <span>History</span></a>
                         </li>
@@ -38,7 +38,7 @@
                             <a href="users1.php"><i class="fa fa-user"></i> <span>Manage Users</span></a>
                         </li>
                         <li>
-                            <a href="rescue.php"><i class="	fa fa-calendar-o"></i> <span>Rescue</span></a> 
+                            <a href="rescue.php"><i class="fa fa-calendar-o"></i> <span>Rescue</span></a> 
                         </li>
                         <li>
                             <a href="logout.php"><i class="fa fa-power-off"></i> <span>Logout</span></a>
@@ -46,7 +46,7 @@
                     </ul>
                 </div>
             </div>
-        </div>    
+        </div>
             <div class="page-wrapper">
                 <div class="content">
                 <div class="row">
