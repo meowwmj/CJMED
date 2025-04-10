f4c0f1ea9dd94edfd0df96e68ec40468fce47d32
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-10 12:12:44 +0800

    Update index.php

--- portal/admin/index.php
+++ portal/admin/index.php
@@ -90,7 +90,7 @@
                     <div class="col-12 col-md-12 col-lg-8 col-xl-8">
                       <div class="card member-panel">
                         <div class="card-header bg-white">
-                                    <h4 class="card-title d-inline-block">Agency</h4> <a href="agency.php" class="btn btn-primary float-right">View all</a>
+                                    <h4 class="card-title d-inline-block">Agency</h4>
                                 </div>
                                 <div class="card-body p-0">
                                     <div class="table-responsive">
@@ -132,7 +132,7 @@
                     <div class="col-12 col-md-12 col-lg-4 col-xl-4">
                         <div class="card member-panel">
                             <div class="card-header bg-white">
-                                    <h4 class="card-title d-inline-block">Users</h4> <a href="users1.php" class="btn btn-primary float-right">View all</a>
+                                    <h4 class="card-title d-inline-block">Users</h4>
                                 </div>
                             <div class="card-body p-0">
                                 <table class="table table-striped">
