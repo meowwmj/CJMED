a0bac5406c933eb34beb48362b3a831017a52693
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-10 08:57:48 +0800

    Update view-emergency.php

--- portal/admin/view-emergency.php
+++ portal/admin/view-emergency.php
@@ -7,8 +7,7 @@
             <div class="sidebar-inner slimscroll">
                 <div id="sidebar-menu" class="sidebar-menu">
                     <ul>
-
-                        <li class="">
+                        <li class>
                             <a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
                         </li>
                         <li>
@@ -16,10 +15,7 @@
                         </li>
                         <li>
                             <a href="agency.php"><i class="fa fa-user-md"></i> <span>Agency</span></a>
-                        </li>
-                        <li>
-                            <a href="report-emergency.php"><i class="fa fa-heartbeat"></i> <span>Reports Emergency</span></a>
-                        </li>
+                        </li>                        
                         <li class="active">
                         <?php
                         $result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending' ORDER BY id DESC ");
