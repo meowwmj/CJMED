124d20bc55dc7d8d4203921e83d7e7c1d6146c3d
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-11 18:40:10 +0800

    Update edit_profile.php

--- portal/admin/edit_profile.php
+++ portal/admin/edit_profile.php
@@ -157,7 +157,7 @@
                                                 </div>
                                             </div>
                                         </div>
-                                        
+                                        <div class="row">
                                         <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Profile Picture</label>
