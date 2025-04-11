e8bffad3d2781075179d3d7f8a2f6127e630f779
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-11 18:34:40 +0800

    Update edit_profile.php

--- portal/admin/edit_profile.php
+++ portal/admin/edit_profile.php
@@ -53,159 +53,134 @@
                 </div>
             </div>
         </div>    
-        <div class="page-wrapper">
+
+       <div class="page-wrapper">
             <div class="content">
                 <div class="row">
-                    <div class="col-lg-8 offset-lg-2">
-                        <h4 class="page-title">Emergency Details</h4>
+                    <div class="col-sm-7 col-6">
+                        <h4 class="page-title">Edit Profile</h4>
                     </div>
-                </div> 
-                <?php if(get("success")):?>
-                    <div>
-                      <?=App::message("success", "Your request has been successfully submitted help is on the way")?>
+
+                    <?php
+                            // Fetch current user details from the session or database
+                            $user_id = $_SESSION['SESS_MEMBER_ID'];
+                            $first_name = $_SESSION['SESS_FIRST_NAME'];
+                            $phone_number = $_SESSION['SESS_PHONE_NUMBER'];
+                            $email = $_SESSION['SESS_EMAIL'];
+                            $address = $_SESSION['SESS_ADDRESS'];
+                            $profile_pic = $_SESSION['SESS_PRO_PIC'];
+
+                            // Handle form submission for updating profile
+                            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                                // Sanitize and process form input data
+                                $new_first_name = htmlspecialchars($_POST['first_name']);
+                                $new_phone_number = htmlspecialchars($_POST['phone_number']);
+                                $new_email = htmlspecialchars($_POST['email']);
+                                $new_address = htmlspecialchars($_POST['address']);
+
+                                // Handle profile picture upload (if a new image is uploaded)
+                                if ($_FILES['profile_pic']['error'] == 0) {
+                                    $target_dir = "uploads/";
+                                    $target_file = $target_dir . basename($_FILES["profile_pic"]["name"]);
+                                    move_uploaded_file($_FILES["profile_pic"]["tmp_name"], $target_file);
+                                    $new_profile_pic = basename($_FILES["profile_pic"]["name"]);
+                                } else {
+                                    $new_profile_pic = $profile_pic;
+                                }
+
+                                // Update session variables
+                                $_SESSION['SESS_FIRST_NAME'] = $new_first_name;
+                                $_SESSION['SESS_PHONE_NUMBER'] = $new_phone_number;
+                                $_SESSION['SESS_EMAIL'] = $new_email;
+                                $_SESSION['SESS_ADDRESS'] = $new_address;
+                                $_SESSION['SESS_PRO_PIC'] = $new_profile_pic;
+
+                                // Assuming you want to update the database
+                                // You can perform a database update query here to persist the changes
+                                // Example:
+                                // $update_query = "UPDATE users SET first_name = ?, phone_number = ?, email = ?, address = ?, profile_pic = ? WHERE id = ?";
+                                // $stmt = $db->prepare($update_query);
+                                // $stmt->execute([$new_first_name, $new_phone_number, $new_email, $new_address, $new_profile_pic, $user_id]);
+
+                                echo '<div class="alert alert-success">Profile updated successfully!</div>';
+                            }
+                        ?>
+
+
+                    <div class="col-sm-5 col-6 text-right m-b-30">
+                        <a href="profile.php" class="btn btn-primary btn-rounded"><i class="fa fa-user"></i> View Profile</a>
                     </div>
-                    <?php endif;?>
-                <div class="row">
-                    <div class="col-lg-8 offset-lg-2">
-                        <?php
-               $id=$_GET['id'];
-    $result = $db->prepare("SELECT * FROM admin where id= :post_id");
-    $result->bindParam(':post_id', $id);
-    $result->execute();
-    for($i=0; $row = $result->fetch(); $i++){                        
-?>
-<form action="update_admin.php" method="post" enctype="multipart/form-data"> 
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Admin ID</label>
-                                        <input class="form-control" type="text"  value="<?php echo $_SESSION['SESS_AGENCY_ID']; ?>">
+                </div>
+                <div class="card-box profile-header">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="profile-view">
+                                <div class="profile-img-wrap">
+                                    <div class="profile-img">
+                                    <?php
+                                    if (!empty($_SESSION['SESS_PRO_PIC'])) {
+                                        echo '<img class="rounded-circle" src="../../uploads/' . $_SESSION['SESS_PRO_PIC'] . '" width="24" height="24">';
+                                    } else {
+                                        echo '<img class="rounded-circle" src="../../uploads/default.jpg" width="24" height="24">';
+                                    }
+                                    ?>                                
                                     </div>
                                 </div>
-                                
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Full Name</label>
-                                       <input class="form-control" type="text" name="name"  value="<?php echo $row['name']; ?>">
-                                        
-                                    </div>
-
-                                </div>
-                            
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    
-                                    <div class="form-group">
-                                        <label>Email </label>
-                                        <input class="form-control" type="text" name="email" value="<?php echo $row['email']; ?>">
-                                    </div>
-                                
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Phone Number  </label>
-                                       <input class="form-control" type="text" name="phone"  value="<?php echo $row['phone']; ?>">
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>State</label>
-                                        <input class="form-control" type="text"  value="<?php echo $row['state']; ?>">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                        <label>Upload image of emergency</label>
-                                        <div class="profile-upload">
-                                            <div class="upload-img">
-                                                <img alt="" src="../../uploads/<?php echo $_SESSION['SESS_PRO_PIC'];?>">
+                                <div class="profile-basic">
+                                    <form action="edit_profile.php" method="POST" enctype="multipart/form-data">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label>First Name</label>
+                                                    <input class="form-control" type="text" name="first_name" value="<?php echo $first_name; ?>" required>
+                                                </div>
                                             </div>
-                                            <div class="upload-input">
-                                                <input type="file" name="photo" class="form-control" >
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label>Phone Number</label>
+                                                    <input class="form-control" type="text" name="phone_number" value="<?php echo $phone_number; ?>" required>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                            </div>
 
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label>Email</label>
+                                                    <input class="form-control" type="email" name="email" value="<?php echo $email; ?>" required>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label>Address</label>
+                                                    <input class="form-control" type="text" name="address" value="<?php echo $address; ?>" required>
+                                                </div>
+                                            </div>
+                                        </div>
 
-                               
-                         <div class="form-group">
-                            <label>Address</label>
-                            <textarea cols="30" rows="4" name="address" class="form-control"><?php echo $row['address']; ?></textarea>
+                                        <div class="form-group">
+                                            <label>Profile Picture</label>
+                                            <input class="form-control" type="file" name="profile_pic">
+                                        </div>
+
+                                        <div class="m-t-20 text-center">
+                                            <button class="btn btn-primary submit-btn">Update Profile</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>                        
                         </div>
-
-                            
-
-                                
-                                <br>
-                            
-
-
-                             
-                            
-                            <div class="m-t-20 text-center">
-                                <button class="btn btn-primary submit-btn">Update Status</button>
-                            </div>
-                          
-
-
-                        </form>
-                    <?php } ?>
                     </div>
                 </div>
             </div>
-            <?php include 'includes/message.php'; ?>
         </div>
     </div>
 
-
-<!-- Logout Confirmation Modal -->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="logoutModalLabel">Confirm Logout</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        Are you sure you want to log out?
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-        <a href="logout.php" class="btn btn-danger">Logout</a>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-    <div class="sidebar-overlay" data-reff=""></div>
-    <script src="assets/js/jquery-3.2.1.min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/jquery.slimscroll.js"></script>
-    <script src="assets/js/select2.min.js"></script>
-    <script src="assets/js/moment.min.js"></script>
-    <script src="assets/js/bootstrap-datetimepicker.min.js"></script>
-    <script src="assets/js/app.js"></script>
-    <script>
-            $(function () {
-                $('#datetimepicker3').datetimepicker({
-                    format: 'LT'
-
-                });
-            });
-     </script>
-
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
 
 <style>
 .submenu-list {
