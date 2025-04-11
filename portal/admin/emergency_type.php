478d8897cda140d7b19b656ac88c7c2abec4f767
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-11 13:40:53 +0800

    Update emergency_type.php

--- portal/admin/emergency_type.php
+++ portal/admin/emergency_type.php
@@ -1,4 +1,5 @@
 <?php include 'includes/head.php'; ?>
+
 <body>
     <div class="main-wrapper">
         <?php include 'includes/navigation.php'; ?>
@@ -12,38 +13,37 @@
                         <li>
                             <a href="announcement.php"><i class="fa fa-bell"></i> <span>Announcements</span></a>
                         </li>
-                        <li class>
-                            <a href="agency.php"><i class="fa fa-user-md"></i> <span>Agency</span></a>
-                        </li>                     
-                        <?php
-                        // include('../connect.php');
-                        $result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending'");
-                        $result->execute();
-                        for($i=0; $row = $result->fetch(); $i++){
-                        ?>  
                         <li>
-                            <a href="view-emergency.php"><i class="fa fa-file"></i> <span>Emergency</span> <span class="badge badge-pill btn-primary float-right"><?php echo $row['total'] ;?></span></a>
+                            <a href="agency.php"><i class="fa fa-user-md"></i> <span>Agency</span></a>
+                        </li>                        
+                        <li class="active">
+                        <?php
+                        $result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending' ORDER BY id DESC ");
+                        $result->execute();
+                        for($i=0; $row = $result->fetch(); $i++){ ?>
+                        <li class="active"><a href="view-emergency.php"><i class="fa fa-file"></i> <span>Emergency</span> <span class="badge badge-pill bg-primary float-right"><?php echo $row['total'] ;?></span></a></li>
+                        <?php } ?>
                         </li>
-                    	<?php } ?>
+                        
                         <li>
                             <a href="report_history.php"><i class="fa fa-file-text-o"></i> <span>History</span></a>
                         </li>
                         <li>                          
                             <a href="view-archived-emergencies.php"><i class="fa fa-archive"></i> <span>Archived</span></a>
                         </li>
-                       <li>
-                            <a href="#"><i class="fa fa-users"></i> <span>Manage</span> <span class="menu-arrow"></span></a>
-                            <ul class="submenu-list">
-                                <li>
-                                    <a href="users.php"><i class="fa fa-user-plus"></i> <span>Manage Admin</span></a>
-                                </li>
-                                <li>
-                                    <a href="users1.php"><i class="fa fa-user"></i> <span>Manage Users</span></a>                        
-                                </li>
-                            </ul>
+                        <li>
+                        <a href="#"><i class="fa fa-users"></i> <span>Manage</span> <span class="menu-arrow"></span></a>
+                        <ul class="submenu-list">
+                            <li>
+                                <a href="users.php"><i class="fa fa-user-plus"></i> <span>Manage Admin</span></a>
+                            </li>
+                            <li>
+                                <a href="users1.php"><i class="fa fa-user"></i> <span>Manage Users</span></a>                        
+                            </li>
+                        </ul>
                         </li>
                         <li>
-                            <a href="rescue.php"><i class="fa fa-calendar-o"></i> <span>Rescue</span></a> 
+                            <a href="rescue.php"><i class="	fa fa-calendar-o"></i> <span>Rescue</span></a> 
                         </li>
                         <li>
                             <a href="logout.php"><i class="fa fa-power-off"></i> <span>Logout</span></a>
@@ -51,7 +51,9 @@
                     </ul>
                 </div>
             </div>
-        </div>
+        </div>    
+    </div>
+
             <div class="page-wrapper">
                 <div class="content">
                 <div class="row">
