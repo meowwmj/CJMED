aa970a75a063c164f4153caae06f728d228338cb
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-11 14:40:02 +0800

    Update emergency_type.php

--- portal/agency/emergency_type.php
+++ portal/agency/emergency_type.php
@@ -40,60 +40,102 @@
             </div>
         </div>
          
-   <div class="page-wrapper">
-            <div class="content">
+     <div class="page-wrapper">
+                <div class="content">
                 <div class="row">
                     <div class="col-lg-8 offset-lg-2">
-                        <h4 class="page-title">Add Department</h4>
+                        <h4 class="page-title">Add Emergency Category</h4>
                     </div>
                 </div>
-                <?php
-                if (isset($_GET['id']) && is_numeric($_GET['id'])) {
-                    $id = $_GET['id'];
-
-                    $result = $db->prepare("SELECT * FROM emergency_type WHERE id = :post_id");
-                    $result->bindParam(':post_id', $id);
-                    $result->execute();
-
-                    if ($row = $result->fetch()) {
-                ?>
-                    <div class="row">
-                        <div class="col-lg-8 offset-lg-2">
-                            <form method="POST" action="update_emergency_type.php">
-                                <input type="hidden" name="id" value="<?php echo $id; ?>">
-
-                                <div class="form-group">
-                                    <label>Department Name</label>
-                                    <input class="form-control" name="name" value="<?php echo htmlspecialchars($row['name']); ?>" type="text" required>
-                                </div>
-
-                                <div class="form-group">
-                                    <label>Description</label>
-                                    <textarea name="description" cols="30" rows="4" class="form-control"><?php echo htmlspecialchars($row['description']); ?></textarea>
-                                </div>
-
-                                <div class="m-t-20 text-center">
-                                    <button class="btn btn-primary submit-btn">Update Department</button>
-                                </div>
-                            </form>
+                     
+                <div class="row">
+                    <div class="col-lg-8 offset-lg-2">
+                        <form method="post" action="save_type.php" enctype="multipart/form-data">
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input class="form-control" name="name" type="text">
+                            </div>
+                            <div class="form-group">
+                                <label>Description</label>
+                                <textarea cols="30" rows="4" name="description" class="form-control"></textarea>
+                            </div>
+                            
+                            <div class="m-t-20 text-center">
+                                <button class="btn btn-primary submit-btn">Save</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="content">
+                <div class="row">
+                    <div class="col-sm-5 col-5">
+                        <h4 class="page-title">All Emergency Categories</h4>
+                    </div>
+                    
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="table-responsive">
+                            <table class="table table-striped custom-table mb-0 datatable">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name</th>
+                                        <th>Description</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                        $result = $db->prepare("SELECT * FROM emergency_type ");
+                                        $result->execute();
+                                        for($i=1; $row = $result->fetch(); $i++){ 
+                                    
+                                    ?> 
+                                    <tr>
+                                        <td><?php echo $i;?></td>
+                                        <td><?php echo $row['name']; ?></td>
+										<td><?php echo $row['description']; ?></td>
+                                        <td class="text-right">
+                                            <div class="dropdown dropdown-action">
+                                                <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
+                                                <div class="dropdown-menu dropdown-menu-right">
+                                                    <a class="dropdown-item" href="edit_emergency_type.php?id=<?php echo $row['id'];?>"><i class="fa fa-pencil m-r-5"></i> Edit</a>
+                                                    <a class="dropdown-item" href="delete_type.php?id=<?=$row['id'] ?>"><i class="fa fa-trash-o m-r-5"></i> Delete</a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
+                                    
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-                <?php
-                    } else {
-                        echo "<div class='alert alert-warning'>No department found with that ID.</div>";
-                    }
-                } else {
-                    echo "<div class='alert alert-danger'>Invalid access. No ID provided.</div>";
-                }
-                ?>
-
+                </div>
             </div>
-			<?php include 'includes/message.php'; ?>
+            <?php include 'includes/message.php'; ?>
         </div>
+		<div id="delete_department" class="modal fade delete-modal" role="dialog">
+			<div class="modal-dialog modal-dialog-centered">
+				<div class="modal-content">
+					<div class="modal-body text-center">
+						<img src="assets/img/sent.png" alt="" width="50" height="46">
+						<h3>Are you sure want to delete this Emergency type?</h3>
+						<div class="m-t-20"> <a href="#" class="btn btn-white" data-dismiss="modal">Close</a>
+                            <a href="delete_type.php?id=<?=$row['id'] ?>" class="btn btn-danger">Delete</a>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
     </div>
 
 
-    <!-- Logout Confirmation Modal -->
+
+<!-- Logout Confirmation Modal -->
 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -114,13 +156,13 @@
   </div>
 </div>
 
-
     <div class="sidebar-overlay" data-reff=""></div>
     <script src="assets/js/jquery-3.2.1.min.js"></script>
-	<script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.dataTables.min.js"></script>
+    <script src="assets/js/dataTables.bootstrap4.min.js"></script>
     <script src="assets/js/jquery.slimscroll.js"></script>
-    <script src="assets/js/select2.min.js"></script>
     <script src="assets/js/app.js"></script>
 
 </body>
