2a543422e095c0292ed9cbf0be61ba1b8cd3f590
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-13 23:51:41 +0800

    Update view-emergency.php

--- portal/users/view-emergency.php
+++ portal/users/view-emergency.php
@@ -43,9 +43,6 @@
                 <div class="col-sm-4 col-3">
                     <h4 class="page-title">Emergency Incident Reports</h4>
                 </div>
-                <div class="col-sm-8 col-9 text-right m-b-20">
-                    <a href="report-emergency.php" class="btn btn-primary btn-rounded float-right"><i class="fa fa-plus"></i> Report Emergency</a>
-                </div>
             </div>
 
             <!-- Filters Section -->
