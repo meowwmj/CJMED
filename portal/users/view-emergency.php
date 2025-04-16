b159b4383a60a00f405624932e1245d3743bbfac
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-16 14:58:21 +0800

    Update view-emergency.php

--- portal/users/view-emergency.php
+++ portal/users/view-emergency.php
@@ -118,8 +118,7 @@
                                                 </td>
                                                 <td class="text-center"><?php echo $row['created_at']; ?></td>
                                                 <td class="text-center">
-                                                    <a class="btn btn-primary" href="make_action.php?id=<?php echo $row['id']; ?>"><i class="fa fa-eye"></i></a>
-                                                    <a class="btn btn-danger" href="delete_emergency.php?id=<?php echo $row['id']; ?>"><i class="fa fa-trash-o"></i></a>
+                                                    <a class="btn btn-primary" href="make_action.php?id=<?php echo $row['id']; ?>"><i class="fa fa-eye"></i></a>                                    
                                                 </td>
                                             </tr>
                                         <?php } ?>
