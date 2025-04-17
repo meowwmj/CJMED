1ff4fbc2371de1a9238b38627758633ccbc9d0f4
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-17 18:11:27 +0800

    Update view-emergency.php

--- portal/agency/view-emergency.php
+++ portal/agency/view-emergency.php
@@ -52,12 +52,13 @@
                 </div>
             </div>
 
-            <!-- Filters Section -->
+           <!-- Filters Section -->
             <div class="row">
                 <div class="col-md-4">
                     <div class="form-group">
                         <select class="form-control" name="status" id="status">
                             <option value="">All</option>
+                            <option value="Reported">Reported</option>
                             <option value="Pending">Ongoing</option>
                             <option value="Resolved">Resolved</option>
                         </select>
