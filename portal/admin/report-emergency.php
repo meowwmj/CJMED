74083bcac89635f1c4429ea232e7749449a557a8
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-19 13:37:09 +0800

    Update report-emergency.php

--- portal/admin/report-emergency.php
+++ portal/admin/report-emergency.php
@@ -84,7 +84,7 @@ date_default_timezone_set('Asia/Manila');
                                 </div>
 
                                 <div class="form-group">
-                                    <label>Patient's Name</label>
+                                    <label>Admin's Name</label>
                                     <input class="form-control" type="text" name="patient_name" value="<?= $_SESSION['SESS_FIRST_NAME'] ?>" readonly>
                                 </div>
 
