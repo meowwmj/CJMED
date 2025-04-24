a2fed504b4a76a25abc66b924efdbf0d5f88a2c4
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-25 04:02:31 +0800

    Update view-emergency.php

--- portal/agency/view-emergency.php
+++ portal/agency/view-emergency.php
@@ -27,19 +27,31 @@ $agency_id = $_SESSION['SESS_AGENCY_ID'];
             <div class="sidebar-inner slimscroll">
                 <div id="sidebar-menu" class="sidebar-menu">
                     <ul>
-                        <li><a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-                        <li><a href="announcement.php"><i class="fa fa-bell"></i> <span>Announcements</span></a></li>
+                        <li>
+                            <a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
+                        </li>
+                        <li>
+                            <a href="announcement.php"><i class="fa fa-bell"></i> <span>Announcements</span></a>
+                        </li>                       
                         <?php
-                            $result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending'");
-                            $result->execute();
-                            for($i=0; $row = $result->fetch(); $i++){
+                        // include('../connect.php');
+                        $result = $db->prepare("SELECT count(*) as total FROM emergency WHERE status = 'Pending'");
+                        $result->execute();
+                        for($i=0; $row = $result->fetch(); $i++){
                         ?>  
-                        <li class="active"><a href="view-emergency.php"><i class="fa fa-file"></i> <span>Emergency</span> <span class="badge badge-pill bg-primary float-right"><?php echo $row['total']; ?></span></a></li>
-                        <?php } ?>
-                        <li><a href="report_history.php"><i class="fa fa-file-text-o"></i> <span>History</span></a></li>
-                        <li><a href="view-archived-emergencies.php"><i class="fa fa-archive"></i> <span>Archived</span></a></li>
-                        <li><a href="rescue.php"><i class="fa fa-calendar-o"></i> <span>Rescue</span></a></li>
-                        <li><a href="logout.php"><i class="fa fa-power-off"></i> <span>Logout</span></a></li>
+                     <li class="active">
+                            <a href="view-emergency.php"><i class="fa fa-file"></i> <span>Emergency</span> <span class="badge badge-pill bg-primary float-right"><?php echo $row['total'] ;?></span></a>
+                        </li>
+                    <?php } ?>
+                        <li >
+                            <a href="report_history.php"><i class="fa fa-file-text-o"></i> <span>History</span></a>
+                        </li>
+                        <li class>                          
+                            <a href="view-archived-emergencies.php"><i class="fa fa-archive"></i> <span>Archived</span></a>
+                        </li> 
+                        <li>
+                            <a href="logout.php"><i class="fa fa-power-off"></i> <span>Logout</span></a>
+                        </li>
                     </ul>
                 </div>
             </div>
