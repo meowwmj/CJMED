ba997ecb003ea480ebe1829133966c199d6f4416
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-26 15:22:16 +0800

    Update report-emergency.php

--- portal/users/report-emergency.php
+++ portal/users/report-emergency.php
@@ -92,7 +92,7 @@ date_default_timezone_set('Asia/Manila');
 
                                     <div class="form-group">
                                         <label>Emergency Category</label>
-                                        <select class="select" name="emergency_category">
+                                        <select class="form-control" name="emergency_category">
                                             <option>Select</option>
                                                 <?php
                                                     $result = $db->prepare("SELECT * FROM emergency_type");
@@ -106,7 +106,7 @@ date_default_timezone_set('Asia/Manila');
                                     
                                     <div class="form-group">
                                         <label>Agency Name</label>
-                                        <select class="select" name="agency_id">
+                                        <select class="form-control" name="agency_id">
                                         <option>Select</option>
                                             <?php
                                             $result = $db->prepare("SELECT * FROM agency");
