ecf31df9b5cdfc3434a7c7bf55e7764cddb66835
meowwmj <158538145+meowwmj@users.noreply.github.com>
2025-04-29 22:56:08 +0800

    Update make_action.php

--- portal/agency/make_action.php
+++ portal/agency/make_action.php
@@ -111,6 +111,8 @@ date_default_timezone_set('Asia/Manila');
                                 <button class="btn btn-primary submit-btn">Update Status</button>
                             </div>
                         </div>
+                    </div>
+                </div>
 
                                     <!-- Modal for displaying the large photo -->
                                     <div class="modal fade" id="photoModal" tabindex="-1" role="dialog" aria-labelledby="photoModalLabel" aria-hidden="true">
